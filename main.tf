e37f7b098cc4a31956f4174159d0e69400eec347
Jaiditya Batra <vampconnoisseur@gmail.com>
2025-02-23 22:56:02 +0530

    19

--- main.tf
+++ main.tf
@@ -11,15 +11,19 @@ data "aws_instances" "existing_flask_server" {
   }
 }
 
-data "aws_security_group" "existing_flask_sg" {
+data "aws_security_groups" "existing_flask_sg" {
   filter {
     name   = "group-name"
     values = ["flask_sg"]
   }
 }
 
+locals {
+  flask_sg_exists = length(try(data.aws_security_groups.existing_flask_sg.ids, [])) > 0
+}
+
 resource "aws_security_group" "flask_sg" {
-  count = length(try(data.aws_security_groups.existing_flask_sg.ids, [])) == 0 ? 1 : 0
+  count = local.flask_sg_exists ? 0 : 1
 
   name        = "flask_sg"
   description = "Allow inbound traffic to Flask app"
@@ -46,39 +50,18 @@ resource "aws_security_group" "flask_sg" {
   }
 }
 
-resource "null_resource" "update_flask_server" {
-  count = length(data.aws_instances.existing_flask_server.ids) > 0 ? 1 : 0
-
-  connection {
-    type        = "ssh"
-    user        = "ubuntu"
-    private_key = file("./my_key.pem")
-    host        = data.aws_instances.existing_flask_server.public_ips[0]
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "echo 'Updating Flask server container...'",
-
-      "if ! command -v docker &> /dev/null; then sudo apt-get update -y && sudo apt-get install -y docker.io; sudo systemctl start docker; sudo systemctl enable docker; fi",
-
-      "if sudo docker ps -a --format '{{.Names}}' | grep -q '^flask-container$'; then sudo docker stop flask-container && sudo docker rm flask-container; fi",
-
-      "sudo docker pull vampconnoisseur/flask-server:${var.image_tag}",
-
-      "sudo docker run -d --name flask-container -p 8765:8765 vampconnoisseur/flask-server:${var.image_tag}"
-    ]
-  }
+locals {
+  flask_server_exists = length(data.aws_instances.existing_flask_server.ids) > 0
 }
 
 resource "aws_instance" "flask_server" {
-  count = length(try(data.aws_instances.existing_flask_server.ids, [])) == 0 ? 1 : 0
+  count = local.flask_server_exists ? 0 : 1
 
   ami           = "ami-04b4f1a9cf54c11d0"
   instance_type = "t2.micro"
   key_name      = "my_key"
 
-  security_groups = aws_security_group.flask_sg.*.name
+  security_groups = local.flask_sg_exists ? [data.aws_security_groups.existing_flask_sg.id] : aws_security_group.flask_sg[*].name
 
   user_data = <<-EOF
             #!/bin/bash
@@ -104,8 +87,33 @@ resource "aws_instance" "flask_server" {
   }
 }
 
+resource "null_resource" "update_flask_server" {
+  count = local.flask_server_exists ? 1 : 0
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("./my_key.pem")
+    host        = data.aws_instances.existing_flask_server.public_ips[0]
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo 'Updating Flask server container...'",
+
+      "if ! command -v docker &> /dev/null; then sudo apt-get update -y && sudo apt-get install -y docker.io; sudo systemctl start docker; sudo systemctl enable docker; fi",
+
+      "if sudo docker ps -a --format '{{.Names}}' | grep -q '^flask-container$'; then sudo docker stop flask-container && sudo docker rm flask-container; fi",
+
+      "sudo docker pull vampconnoisseur/flask-server:${var.image_tag}",
+
+      "sudo docker run -d --name flask-container -p 8765:8765 vampconnoisseur/flask-server:${var.image_tag}"
+    ]
+  }
+}
+
 output "public_ip" {
-  value = length(data.aws_instances.existing_flask_server.ids) > 0 ? data.aws_instances.existing_flask_server.public_ips[0] : aws_instance.flask_server[0].public_ip
+  value = local.flask_server_exists ? data.aws_instances.existing_flask_server.public_ips[0] : aws_instance.flask_server[0].public_ip
 }
 
 terraform {
